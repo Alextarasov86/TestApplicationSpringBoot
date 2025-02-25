d813da1412936c53c89c14de1ce56322fdc93439
Alex <alex.t.86@mail.ru>
2025-02-25 17:22:42 +0300

    commit 2

---
+++ catalogue-service/src/main/java/ru/alex/catalogue/controller/ProductsRestController.java
@@ -0,0 +1,50 @@
+package ru.alex.catalogue.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.alex.catalogue.controller.payload.NewProductPayload;
+import ru.alex.catalogue.entity.Product;
+import ru.alex.catalogue.service.ProductService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("catalogue-api/products")
+public class ProductsRestController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> findProducts() {
+        return this.productService.findAllProducts();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
+                                                BindingResult bindingResult,
+                                                 UriComponentsBuilder uriBuilder) throws BindException {
+        if (bindingResult.hasErrors()) {
+            if(bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
+        } else{
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return ResponseEntity.created(uriBuilder.replacePath("/catalogue/products/{productId}").
+                            build(Map.of("productId", product.getId()))).
+                    body(product);
+        }
+    }
+
+
+}
---
+++ manager-app/src/main/java/ru/alex/manager/entity/Product.java
@@ -0,0 +1,4 @@
+package ru.alex.manager.entity;
+
+public record Product(int id, String title, String details) {
+}
